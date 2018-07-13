d36a14b26780c5f48f1104c17aaa40f9b7780300
ssunhye <33833780+ssunhye@users.noreply.github.com>
2018-07-13 11:24:19 +0900

    Add files via upload

---
+++ assets/js/core/main.js
@@ -0,0 +1,89 @@
+var audioContext = null;
+var meter = null;
+var canvasContext = null;
+var WIDTH=500;
+var HEIGHT=50;
+var rafID = null;
+
+window.onload = function() {
+    // monkeypatch Web Audio
+    window.AudioContext = window.AudioContext || window.webkitAudioContext;
+	
+    // grab an audio context
+    audioContext = new AudioContext();
+
+    // Attempt to get audio input
+    try {
+        // monkeypatch getUserMedia
+        navigator.getUserMedia = 
+        	navigator.getUserMedia ||
+        	navigator.webkitGetUserMedia ||
+        	navigator.mozGetUserMedia;
+
+        // ask for an audio input
+        navigator.getUserMedia(
+        {
+            "audio": {
+                "mandatory": {
+                    "googEchoCancellation": "false",
+                    "googAutoGainControl": "false",
+                    "googNoiseSuppression": "false",
+                    "googHighpassFilter": "false"
+                },
+                "optional": []
+            },
+        }, gotStream, didntGetStream);
+    } catch (e) {
+        alert('getUserMedia threw exception :' + e);
+    }
+
+}
+
+function didntGetStream() {
+    alert('Stream generation failed.');
+}
+
+var mediaStreamSource = null;
+var data=0;
+var cnt = 0;
+
+function gotStream(stream) {
+    // Create an AudioNode from the stream.
+    mediaStreamSource = audioContext.createMediaStreamSource(stream);
+
+    // Create a new volume meter and connect it.
+    meter = createAudioMeter(audioContext);
+    mediaStreamSource.connect(meter);
+
+    // kick off the visual updating
+    
+    Plotly.plot('chart',[{
+        y:[data],
+        type:'line'
+    }]);
+
+
+    Plotly.plot('chart2',[{
+        y:[data],
+        type:'line'
+    }]);
+
+    setInterval(function(){
+        data=meter.volume;
+
+        document.getElementById("show").innerHTML = meter.volume;
+        Plotly.extendTraces('chart',{y:[[data]]}, [0]);
+        ++cnt;
+    
+        if(cnt>40){
+            Plotly.relayout('chart',{
+                xaxis: {
+                    range: [cnt-40,cnt]
+                }
+            })
+        }
+
+        Plotly.extendTraces('chart2',{y:[[data]]}, [0]);
+    },0);
+
+}
---
+++ assets/js/core/volume-meter.js
@@ -0,0 +1,54 @@
+function createAudioMeter(audioContext,clipLevel,averaging,clipLag) {
+	var processor = audioContext.createScriptProcessor(512);
+	processor.onaudioprocess = volumeAudioProcess;
+	processor.clipping = false;
+	processor.lastClip = 0;
+	processor.volume = 0;
+	processor.clipLevel = clipLevel || 0.98;
+	processor.averaging = averaging || 0.95;
+	processor.clipLag = clipLag || 750;
+
+	processor.connect(audioContext.destination);
+
+	processor.checkClipping =
+		function(){
+			if (!this.clipping)
+				return false;
+			if ((this.lastClip + this.clipLag) < window.performance.now())
+				this.clipping = false;
+			return this.clipping;
+		};
+
+	processor.shutdown =
+		function(){
+			this.disconnect();
+			this.onaudioprocess = null;
+		};
+
+	return processor;
+}
+
+function volumeAudioProcess( event ) {
+	var buf = event.inputBuffer.getChannelData(0);
+    var bufLength = buf.length;
+	var sum = 0;
+    var x;
+
+	// Do a root-mean-square on the samples: sum up the squares...
+    for (var i=0; i<bufLength; i++) {
+    	x = buf[i];
+    	if (Math.abs(x)>=this.clipLevel) {
+    		this.clipping = true;
+    		this.lastClip = window.performance.now();
+    	}
+    	sum += x * x;
+	}
+
+    // ... then take the square root of the sum.
+	var rms =  Math.sqrt(sum / bufLength);
+	
+	var decibel = 20*(Math.log10(rms/0.000002));
+	
+    //this.volume = Math.max(rms, this.volume*this.averaging);
+	this.volume = decibel;
+}
