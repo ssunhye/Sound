51b70eb527c9162fba92bbaeed9f71f8bd05834e
ssunhye <33833780+ssunhye@users.noreply.github.com>
2018-07-16 17:39:57 +0900

    Add files via upload

--- assets/js/core/volume-meter.js
+++ assets/js/core/volume-meter.js
@@ -28,7 +28,7 @@ function volumeAudioProcess( event ) {
 
 		var rms =  Math.sqrt(sum / bufLength/2);
 		rms=rms.toFixed(5);
-		if(Math.log10(rms/0.000002)!=0){
+		if((rms/0.000002)!=0){
 			var decibel = 20*(Math.log10(rms/0.000002));		
 			this.volume = decibel;
 		}
