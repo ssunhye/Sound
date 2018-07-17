3d85091d0b52e0b9675f5378e0ebc73c838d86c5
ssunhye <33833780+ssunhye@users.noreply.github.com>
2018-07-17 14:18:39 +0900

    Add files via upload

--- assets/js/core/volume-meter.js
+++ assets/js/core/volume-meter.js
@@ -17,23 +17,23 @@ function createAudioMeter(audioContext) {
 function volumeAudioProcess( event ) {
 	var buf = event.inputBuffer.getChannelData(0);
     var bufLength = buf.length;
-	var sum = 0;
+	var sum = 0.00001;
     var x;
 	var maxAmplitude=Math.abs(buf[0]);
 	if(bufLength!=0){
 		for (var i=0; i<bufLength; i++) {
 			x = Math.abs(buf[i]);
-			//sum += x * x;
+			sum += x * x;
 			if(maxAmplitude>x){
 				maxAmplitude=x;
 			}
 		}
 
 		var rms =  Math.sqrt(sum / bufLength/2);
-		rms=rms.toFixed(5);
-		if(x>0){
+		rms=rms.toFixed(6);
+		if(x>0.000001){
 			// var decibel = 20*(Math.log10(rms/0.000002));
-			var decibel = 20*(Math.log10(x/0.000002));		
+			var decibel = 20*(Math.log10(rms/0.000002));		
 			this.volume = decibel;
 		}
 	}
